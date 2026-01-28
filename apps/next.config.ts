5f090db3a3a365c014bf5ae8a3af7daa386a1ac5
Nakul Chouksey <71.nakul@gmail.com>
2026-01-28 22:46:18 +0530

    chore: bump nypm, citty, tinyexec, update node engine to >=18, disable PWA, clean sw.js

--- apps/next.config.ts
+++ apps/next.config.ts
@@ -4,7 +4,7 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: true,
 });
 
 /** @type {import('next').NextConfig} */
