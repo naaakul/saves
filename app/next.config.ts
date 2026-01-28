934b6fec1d038236c9ea2566e9c3563c9459fc51
Nakul Chouksey <71.nakul@gmail.com>
2026-01-28 23:20:52 +0530

    chore: remove PWA plugin from next config

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,24 +1,15 @@
-import withPWAInit from "next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {}, 
+  turbopack: {},
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: "https" as const,
+        protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
     ],
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
