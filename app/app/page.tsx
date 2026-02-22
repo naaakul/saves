fe4fdacc106a26f3a7d7ff67b9248a728a5d243e
Nakul Chouksey <71.nakul@gmail.com>
2026-02-22 22:13:22 +0530

    feat: modify app page component

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -28,7 +28,7 @@ const page = async () => {
       <div className="bg-[#D4D4D4] h-full w-full rounded-2xl flex justify-center flex-col">
         <Navbar  />
         <div className="flex justify-center items center h-9/12 w-full">
-          <div className="w-1/2 h-full text-black flex justify-center flex-col p-16 relative">
+          <div className="w-1/2 h-full text-black pl-24 flex justify-center flex-col py-16 relative">
             <div className="w-[0.100rem] bg-[linear-gradient(to_bottom,_#00000000_0%,_#00000025_25%,_#00000030_50%,_#00000025_75%,_#00000000_100%)] h-10/12 absolute top-3 left-12" />
             <div className="w-[0.100rem] bg-[linear-gradient(to_bottom,_#00000000_0%,_#00000025_25%,_#00000030_50%,_#00000025_75%,_#00000000_100%)] h-10/12 absolute left-32" />
             <div className="w-[0.100rem] bg-[linear-gradient(to_bottom,_#00000000_0%,_#00000025_25%,_#00000030_50%,_#00000025_75%,_#00000000_100%)] h-10/12 absolute top-10 left-[28rem]" />
@@ -57,13 +57,13 @@ const page = async () => {
               </span>
             </p>
             {session?.user ? (
-              <Link href={"/inventory"}>
+              <Link href={"/inventory"} className="w-fit">
                 <Button className="bg-black z-10 px-12 w-fit mt-3 text-lg py-7 rounded-xl shadow-[inset_0px_0px_30px_rgba(255,255,255,0.2),0px_15px_45px_rgba(0,0,0,0.3)] text-white hover:bg-black cursor-pointer">
                   inventory
                 </Button>
               </Link>
             ) : (
-              <Link href={"/auth/login"}>
+              <Link href={"/auth/login"} className="w-fit">
                 <Button className="bg-black z-10 px-12 w-fit mt-3 text-lg py-7 rounded-xl shadow-[inset_0px_0px_30px_rgba(255,255,255,0.2),0px_15px_45px_rgba(0,0,0,0.3)] text-white hover:bg-black cursor-pointer">
                   Sign up now
                 </Button>
